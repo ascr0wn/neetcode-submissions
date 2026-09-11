adfc8cd085f9ed355efb589fea662fab9f0e7861
Aryan Srivastava <aryan.200903.srivastava@gmail.com>
2026-09-11 21:35:51 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,15 @@
+import random
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            random_index = random.randint(left, right)
+            if nums[random_index] == target:
+                return random_index
+            elif target > nums[random_index]:
+                left = random_index + 1
+            else:
+                right = random_index - 1
+        return -1
